375811cdae73cdbd3a28cddf60298f3fea09cded
Milan Darjanin <miland@Milans-MacBook-Pro.local>
2022-01-21 17:14:37 +0100

    Add script generation

--- src/App.svelte
+++ src/App.svelte
@@ -9,6 +9,22 @@
   } from "./util";
 
   let bundles = [];
+  let files;
+  let copied = false;
+
+  const tabs = ["library", "script"];
+  let activeTab = "library";
+
+  function copyShellScript() {
+    const scriptTextarea = document.querySelector("#shellScript");
+    scriptTextarea.select();
+    document.execCommand("copy");
+    copied = true;
+    scriptTextarea.blur();
+    setTimeout(() => {
+      copied = false;
+    }, 1000);
+  }
 
   onMount(async () => {
     let response = await fetch("/library.json");
@@ -23,6 +39,20 @@
   let totalSize = 0;
 
   $: totalSize = formatBytes(calculateTotalSize(mappedProducts(bundles)));
+
+  function readUploadedJson(files) {
+    if (files && files[0]) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        bundles = Object.values(JSON.parse(event.target.result)).map(
+          (value) => value
+        );
+      };
+      reader.readAsText(files[0]);
+    }
+  }
+
+  $: readUploadedJson(files);
 </script>
 
 <main>
@@ -32,19 +62,55 @@
   <p class="headline">
     Total size of downloadable content is {totalSize}.
   </p>
-  <div class="bundles">
-    {#each bundles as bundle}
-      <Card {bundle} />
-    {/each}
-  </div>
-  <!-- 
-  <h2>Your download script</h2>
-  <pre>{generateShellScript(mappedProducts(bundles))}</pre> -->
+  <header>
+    <input type="file" bind:files />
+    <h2>{activeTab}</h2>
+    <div>
+      <button
+        on:click={() => {
+          activeTab = "library";
+        }}>Library</button
+      >
+      <button
+        on:click={() => {
+          activeTab = "script";
+        }}>Download Script</button
+      >
+    </div>
+  </header>
+
+  {#if activeTab === "library"}
+    <div class="bundles">
+      {#each bundles as bundle}
+        <Card {bundle} />
+      {/each}
+    </div>
+  {:else}
+    <header>
+      <h2>Your download script</h2>
+      <button on:click={copyShellScript} disabled={copied}>
+        {#if copied}
+          Copied
+        {:else}
+          Copy to clipboard
+        {/if}
+      </button>
+    </header>
+    <textarea id="shellScript"
+      >{generateShellScript(mappedProducts(bundles))}</textarea
+    >
+  {/if}
 </main>
 
 <style>
   main {
     padding: 1em;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 
   h1 {
@@ -61,9 +127,10 @@
     color: #757575;
   }
 
-  pre {
-    max-height: 500px;
-    overflow-x: auto;
+  textarea {
+    height: 400px;
+    width: 100%;
+    font-family: monospace;
   }
 
   @media (min-width: 640px) {
